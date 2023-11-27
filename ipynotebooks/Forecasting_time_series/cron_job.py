a1f7628837199abb0f6581bad4296d6432994b6a
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2023-11-27 13:36:03 +0530

    added logs

--- ipynotebooks/Forecasting_time_series/cron_job.py
+++ ipynotebooks/Forecasting_time_series/cron_job.py
@@ -3,6 +3,8 @@ import pandas as pd
 import pprint
 import csv
 from datetime import datetime
+import logging
+import os
 
 # NISE
 NISE = "nise gwal"
@@ -28,42 +30,47 @@ VIKAS_SADAN_OUTPUT = "data/cron_job_data/vikas_sadan_cron_output"
 # In 2nd cron daily job, Incase if api fails, write to file with previous day
 
 def setData(station, output_file):
-    url = "https://api.waqi.info/search/?token=7c0e1c5a796cf1a14edf4bf1462a99e9b37d8bdf&keyword=" + station
-    response = requests.get(url)
-    if response.status_code == 200:
-        res = response.json()
-        result = []
-        if station.lower() in (res["data"][0]['station']['name']).lower():
-            result.append(res["data"][0]['aqi'])
-            result.append(res["data"][0]['station']['name'])
-            result.append(pd.to_datetime(res["data"][0]['time']['stime']))
-        print()
-        print(f"station=> {station}, result => {result}")
-        print()
-        # Write to the file only when (station, time) is not already existing in the file.
+    try:
+        # Create a "logs" directory if it doesn't exist
+        logs_directory = "logs"
+        os.makedirs(logs_directory, exist_ok=True)
 
-        new_timestamp = (res["data"][0]['time']['stime'])
-        csv_file_path = output_file + '.csv'
+        # Set up logging configuration
+        log_file_path = os.path.join(logs_directory, f"{datetime.now().strftime('%d-%m-%Y')}.log")
+        logging.basicConfig(filename=log_file_path, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-        # Check if the new timestamp is already present
-        with open(csv_file_path, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            # Assuming the timestamp is in the 3rd column
-            existing_timestamps = [row[2] for row in csv_reader]
+        url = "https://api.waqi.info/search/?token=7c0e1c5a796cf1a14edf4bf1462a99e9b37d8bdf&keyword=" + station
+        response = requests.get(url)
+        if response.status_code == 200:
+            res = response.json()
+            result = []
+            if station.lower() in (res["data"][0]['station']['name']).lower():
+                result.append(res["data"][0]['aqi'])
+                result.append(res["data"][0]['station']['name'])
+                result.append(pd.to_datetime(res["data"][0]['time']['stime']))
+            logging.info(f"station=> {station}, result => {result}")
+            # Write to the file only when (station, time) is not already existing in the file.
 
-        if new_timestamp not in existing_timestamps:
-            with open(csv_file_path, 'a', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(result)
-            print(
-                f'The data has been written to {csv_file_path} with Timestamp: {new_timestamp}'
-            )
+            new_timestamp = (res["data"][0]['time']['stime'])
+            csv_file_path = output_file + '.csv'
+
+            # Check if the new timestamp is already present
+            with open(csv_file_path, 'r') as csv_file:
+                csv_reader = csv.reader(csv_file)
+                # Assuming the timestamp is in the 3rd column
+                existing_timestamps = [row[2] for row in csv_reader]
+
+            if new_timestamp not in existing_timestamps:
+                with open(csv_file_path, 'a', newline='') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow(result)
+                logging.info(f'The data has been written to {csv_file_path} with Timestamp: {new_timestamp}')
+            else:
+                logging.info(f'Timestamp {new_timestamp} already present in {csv_file_path}, not appending.')
         else:
-            print(
-                f'Timestamp {new_timestamp} already present in {csv_file_path}, not appending.'
-            )
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+            logging.info(f"Error: {response.status_code} - {response.text}")
+    except Exception as e:
+        logging.info(f"Exception {e} has occured for station=> {station}, result => {result}")
         
 # Initialize new file with ,,, or else you will get error
 
