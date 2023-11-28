fecba689768da79e211ceb82d6ed92fa66644c7a
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2023-11-28 07:42:41 +0530

    changes in logs

--- ipynotebooks/Forecasting_time_series/cron_job.py
+++ ipynotebooks/Forecasting_time_series/cron_job.py
@@ -5,26 +5,27 @@ import csv
 from datetime import datetime
 import logging
 import os
+import logging.handlers
 
 # NISE
 NISE = "nise gwal"
 NISE_STATION = "NISE Gwal Pahari, Gurugram, India"
-NISE_OUTPUT = "/data/cron_job_data/nise_cron_output"
+NISE_OUTPUT = "../../data/cron_job_data/nise_cron_output"
 
 # Sector 51
 SECTOR_51 = "Sector-51, Gurugram"
 SECTOR_51_STATION = "Sector-51, Gurugram, India"
-SECTOR_51_OUTPUT = "/data/cron_job_data/sector_51_cron_output"
+SECTOR_51_OUTPUT = "../../data/cron_job_data/sector_51_cron_output"
 
 # Teri gram
 TERI_GRAM = "Teri Gram"
 TERI_GRAM_STATION = "Teri Gram, Gurugram"
-TERI_GRAM_OUTPUT = "/data/cron_job_data/teri_gram_cron_output"
+TERI_GRAM_OUTPUT = "../../data/cron_job_data/teri_gram_cron_output"
 
 # Vikas Sadan
 VIKAS_SADAN = "Vikas Sadan"
 VIKAS_SADAN_STATION = "Vikas Sadan Gurgaon"
-VIKAS_SADAN_OUTPUT = "/data/cron_job_data/vikas_sadan_cron_output"
+VIKAS_SADAN_OUTPUT = "../../data/cron_job_data/vikas_sadan_cron_output"
 
 def get_api_token():
     try:
@@ -39,19 +40,30 @@ def get_api_token():
 
 def setData(station, output_file):
     try:
-        print(f"os.getcwd()=> {os.getcwd()}")
-        cwd = os.getcwd()
-        # Create a "logs" directory if it doesn't exist
-        logs_directory = cwd + "/logs"
-        os.makedirs(logs_directory, exist_ok=True)
+        
+#         # Create a "logs" directory if it doesn't exist
+#         logs_directory = "../../logs"
+#         os.makedirs(logs_directory, exist_ok=True)
 
-        # Set up logging configuration
-        log_file_path = os.path.join(logs_directory, f"{datetime.now().strftime('%d-%m-%Y')}.log")
-        logging.basicConfig(filename=log_file_path, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+#         # Set up logger configuration
+#         log_file_path = os.path.join(logs_directory, f"{datetime.now().strftime('%d-%m-%Y')}.log")
+#         logger.basicConfig(filename=log_file_path, level=logger.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-        print(f"log_file_path: {log_file_path}")
+        logger = logging.getLogger(__name__)
+
+        logger.setLevel(logging.DEBUG)
+        logger_file_handler = logging.handlers.RotatingFileHandler(
+            f"../../logs/{datetime.now().strftime('%d-%m-%Y')}.log",
+            maxBytes=1024 * 1024,
+            backupCount=1,
+            encoding="utf8",
+        )
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        logger_file_handler.setFormatter(formatter)
+        logger.addHandler(logger_file_handler)
+        print(f"logger {logger}" )
+        
         TOKEN = get_api_token()
-        print(f'TOKEN: {TOKEN}')
         url = "https://api.waqi.info/search/?token=" + TOKEN + "&keyword=" + station
         response = requests.get(url)
         if response.status_code == 200:
@@ -61,13 +73,11 @@ def setData(station, output_file):
                 result.append(res["data"][0]['aqi'])
                 result.append(res["data"][0]['station']['name'])
                 result.append(pd.to_datetime(res["data"][0]['time']['stime']))
-            print(f"station=> {station}, result => {result}")
-            logging.info(f"station=> {station}, result => {result}")
+            logger.info(f"station=> {station}, result => {result}")
             # Write to the file only when (station, time) is not already existing in the file.
 
             new_timestamp = (res["data"][0]['time']['stime'])
-            csv_file_path = cwd + output_file + '.csv'
-            print(f'csv_file_path=> {csv_file_path}')
+            csv_file_path = output_file + '.csv'
 
             # Check if the new timestamp is already present
             with open(csv_file_path, 'r') as csv_file:
@@ -79,17 +89,14 @@ def setData(station, output_file):
                 with open(csv_file_path, 'a', newline='') as csv_file:
                     csv_writer = csv.writer(csv_file)
                     csv_writer.writerow(result)
-                print(f'The data has been written to {csv_file_path} with Timestamp: {new_timestamp}')
-                logging.info(f'The data has been written to {csv_file_path} with Timestamp: {new_timestamp}')
+                logger.info(f'The data has been written to {csv_file_path} with Timestamp: {new_timestamp}')
             else:
-                print(f'Timestamp {new_timestamp} already present in {csv_file_path}, not appending.')
-                logging.info(f'Timestamp {new_timestamp} already present in {csv_file_path}, not appending.')
+                logger.info(f'Timestamp {new_timestamp} already present in {csv_file_path}, not appending.')
         else:
-            logging.info(f"Error: {response.status_code} - {response.text}")
-            print(f"Error: {response.status_code} - {response.text}")
+            logger.info(f"Error: {response.status_code} - {response.text}")
     except Exception as e:
-        print(f"Exception {type(e).__name__} has occured for station=> {station}")
-        logging.info(f"Exception {type(e).__name__} has occured for station=> {station}")
+        print("Exception ",e)
+        logger.info(f"Exception {type(e).__name__} has occured for station=> {station}")
         
 # Initialize new file with ,,, or else you will get error
 
