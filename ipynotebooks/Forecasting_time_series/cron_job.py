0a22329f9ff4ef58e739aac444c0779dc4c9cb03
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2023-12-29 21:12:02 +0530

    added more logs

--- ipynotebooks/Forecasting_time_series/cron_job.py
+++ ipynotebooks/Forecasting_time_series/cron_job.py
@@ -290,8 +290,12 @@ if __name__ == "__main__":
         setData(station,  station_location, logger, TOKEN)
     
      # If the day changes, append it to original data
-    
+    print(f"datetime.now().hour=> {datetime.now().hour}")
+    logger.info(f"datetime.now().hour=> {datetime.now().hour}")
+
     if datetime.now().hour == 20:     # It means 1 AM IST (20 is GitHub action runner time)
+        print("Calling writeData & retrain_model functions.")
+        logger.info("Calling writeData & retrain_model functions.")
         writeData(SECTOR_51_OUTPUT, SECTOR_51_DAILY_AQI)
         retrain_model(ORDER, SEASONAL_ORDER, SECTOR_51_DAILY_AQI)
 #         for station, station_location in stations:
